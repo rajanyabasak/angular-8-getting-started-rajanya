ebc2b87b112d81a94794f51f8d4d9f64bd7cf4dd
rajanyabasak <141635686+rajanyabasak@users.noreply.github.com>
2023-08-07 22:02:10 +0530

    1 file modified

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,2 +1,23 @@
-<p>children works!</p>
-<div>{{this.id}}</div>
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-children',
+  templateUrl: './children.component.html',
+  styleUrls: ['./children.component.css'],
+})
+export class ChildrenComponent implements OnInit {
+  name: string;
+  id: number;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.id = +params['id'];
+      this.name = params['name'];
+    });
+
+    console.log(this.id);
+  }
+}
