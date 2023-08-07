d73ca489d3b9b1f7339e30a4bc928754b74320e5
rajanyabasak <141635686+rajanyabasak@users.noreply.github.com>
2023-08-07 22:09:10 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './parent/parent.component';
 
 @NgModule({
   imports: [
@@ -18,7 +18,7 @@ import { ProductListComponent } from './product-list/product-list.component';
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent
+    ParentComponent
   ],
   bootstrap: [ AppComponent ]
 })
