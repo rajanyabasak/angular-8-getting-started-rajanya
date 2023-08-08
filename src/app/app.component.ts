beb0a10af2cf1fdc08a4a339c78fc35128ab2d2b
rajanyabasak <141635686+rajanyabasak@users.noreply.github.com>
2023-08-08 16:38:18 +0530

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { UserDataService } from './services/user-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {}
+export class AppComponent  {
+
+  users: any;
+  constructor(private userData: UserDataService)
+  {
+    userData.usersData().subscribe((data)=>{
+      console.warn("data", data);
+      this.users=data
+    });
+  }
+}
 
 
 /*
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
@@ -13,9 +14,10 @@ import { ChildComponent } from './component/child/child.component';
     BrowserModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
+     // { path: '', component: AppComponent },
       { path: 'child', component: ChildComponent },
     ]),
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
